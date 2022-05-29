d4c7959f542f00f477a22ff6831927ef17c99a8f
jaqbiak <jaqbiak88@gmail.com>
2022-05-29 13:20:56 +0200

    adding pylint 🫥

--- tests/base_page.py
+++ tests/base_page.py
@@ -2,5 +2,6 @@ class BasePage:
     def __init__(self, driver):
         self._driver = driver
 
-    def _get_title_of_page(self):
+    def get_title_of_page(self):
+        """ This metod returns title of the current page """
         return self._driver.title
--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -5,10 +5,9 @@ from tests.login.login_page import LoginPage
 
 @pytest.mark.login
 class TestLogin:
-
     @staticmethod
     def test_title(browser):
         page = LoginPage(browser)
         expected_title = "Google"
-        actual_title = page._get_title_of_page()
+        actual_title = page.get_title_of_page()
         assert actual_title == expected_title, f"Title should be {expected_title}, but it's {actual_title}"
